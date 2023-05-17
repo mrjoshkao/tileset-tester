2d87e56fb0596bcd5e77f71433db724660a6ad82
mrjoshkao <38117443+mrjoshkao@users.noreply.github.com>
2023-05-16 22:11:33 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,289 @@
+tiles.setCurrentTilemap(tilemap`level1`)
+let Room_Builder_free_16x16_0 = img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `
+let Room_Builder_free_16x16_1 = img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . d . d d . . d d d d . . . . 
+    . d f d f f d d f f f f d . . d 
+    . d f f d d d f d d d d f d d f 
+    . d f d . . d f d . . d f d d f 
+    . d f d . . d f d . . d f d d f 
+    `
+let Room_Builder_free_16x16_2 = img`
+    . d f d . . d f d . . d f d d f 
+    . d f d . . d f d d d d f d d f 
+    . d f d . . . d f f f f d . . d 
+    . . d . . . . . d d d d . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . d 
+    . . . . . . . . . . . . . . . d 
+    . . . . . . . . . . . . . . d f 
+    . . . . . . . . . . . . . . d f 
+    `
+let Room_Builder_free_16x16_3 = img`
+    . . . . . . . . . . . . . d f d 
+    . . . . . . . . . . . . . d f d 
+    . . . . . . . . . . . . . d f d 
+    . . . . . . . . . . . . d f d . 
+    . . . . . . . . . . . . d f d . 
+    . . . . . . . . . . . d f d . . 
+    . . . . . . . . . . . d f d . . 
+    . . . . . . . . . . . . d . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `
+let Room_Builder_free_16x16_4 = img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `
+let Room_Builder_free_16x16_5 = img`
+    c c c c c c c c c c c c c c c c 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c c c c c c c c c c c c c c c c 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    `
+let Room_Builder_free_16x16_6 = img`
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c e e e e e e e e e e e e e e e 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c b b b b b b b b b b b b b b b 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c b b b b b b b b b b b b b b b 
+    c c c c c c c c c c c c c c c c 
+    `
+let Room_Builder_free_16x16_7 = img`
+    c c c c c c c c c c c c c c c c 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c c c c c c c c c c c c c c c c 
+    c 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+    c 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    `
+let Room_Builder_free_16x16_8 = img`
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+    c 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+    c d d d d d d d d d d d d d d d 
+    c 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+    c 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+    c 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+    c b b b b b b b b b b b b b b b 
+    c c c c c c c c c c c c c c c c 
+    `
+let Room_Builder_free_16x16_9 = img`
+    c c c c c c c c c c c c c c c c 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c c c c c c c c c c c c c c c c 
+    c 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    c d d d d d d d d d d d d d d d 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    `
+let Room_Builder_free_16x16_10 = img`
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c d d d d d d d d d d d d d d d 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c d d d d d d d d d d d d d d d 
+    c b b b b b b b b b b b b b b b 
+    c c c c c c c c c c c c c c c c 
+    `
+let Room_Builder_free_16x16_11 = img`
+    c c c c c c c c c c c c c c c c 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c c c c c c c c c c c c c c c c 
+    c 4 4 4 4 4 4 4 c 4 4 4 4 4 4 4 
+    c 4 4 4 4 4 4 4 c 4 4 4 4 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 4 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    `
+let Room_Builder_free_16x16_12 = img`
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 c 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 c 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 c 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 c 4 4 4 4 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 e 4 4 4 e 4 4 4 e 4 4 4 
+    c 4 4 4 4 4 4 4 c 4 4 4 4 4 4 4 
+    c 4 4 4 4 4 4 4 c 4 4 4 4 4 4 4 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c c c c c c c c c c c c c c c c 
+    `
+let Room_Builder_free_16x16_13 = img`
+    c c c c c c c c c c c c c c c c 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c c c c c c c c c c c c c c c c 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    `
+let Room_Builder_free_16x16_14 = img`
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c b b b b b b b b b b b b b b b 
+    c b b b b b b b b b b b b b b b 
+    c c c c c c c c c c c c c c c c 
+    `
+let Room_Builder_free_16x16_15 = img`
+    c c c c c c c c c c c c c c c c 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    c c c c c c c c c c c c c c c c 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e c e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    c e e e e e e e e e e e e e e e 
+    `
